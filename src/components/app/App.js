5475d61dad251d5ccf282955ad8274079e03b041
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2018-11-20 10:37:08 -0700

    "Organization"

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,11 +5,10 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return ( <
-      div className = "App" >
-      MORE COMPONENTS!!!
-      <
-      /div>
+    return (
+      <div className="App" >
+        MORE COMPONENTS!!!
+      </div>
     );
   }
 }
