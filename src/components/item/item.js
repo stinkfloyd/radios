2114bda1ea92cf30b84c506567edf58df9e33940
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2018-11-20 11:25:21 -0700

    Discard button added

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -1,5 +1,14 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Item = ({ item }) => <li>{item.item} {item.reason}</li>
+
+// const Item = ({ item }) => <li> <button>Discard</button> {item.item} {item.reason}</li>
+
+class Item extends Component {
+    render() {
+        const { item } = this.props
+        return <li><button>Discard</button> {item.item} {item.reason} </li>
+    }
+
+}
 
 export default Item
