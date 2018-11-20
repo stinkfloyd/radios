e6a6dd32ee8c81c06e1c9db4a67543a92c06a906
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2018-11-20 10:59:37 -0700

    List on

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,12 +2,24 @@ import React, {
   Component
 } from 'react';
 import './App.css';
+import ItemList from '../item-list/item-list'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        { id: 0, item: 'Old AM radio.', reason: 'Hear crackles in the summer.' },
+        { id: 1, item: 'Shovel', reason: 'Becasue i hate telephone wires.' },
+        { id: 2, item: '2m/70cm dueal band transceiver', reason: 'Because MORE RADIOS!!' }
+      ],
+    }
+  }
   render() {
     return (
       <div className="App" >
         MORE COMPONENTS!!!
+        <ItemList />
       </div>
     );
   }
---
+++ src/components/item-list/item-list.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ItemList = () => {
+    return (
+        <ul>
+            <li> List will go here </li>
+        </ul>
+    )
+}
+
+export default ItemList
